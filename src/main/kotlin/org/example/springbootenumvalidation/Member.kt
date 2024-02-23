0578856865edb95419be4cc21cb80c1416fa116e
chris.han <namilhan@gmail.com>
2024-02-24 00:03:51 +0900

    사용하지 않는 코드 제거

--- src/main/kotlin/org/example/springbootenumvalidation/Member.kt
+++ src/main/kotlin/org/example/springbootenumvalidation/Member.kt
@@ -6,9 +6,7 @@ data class Member(
   @field:NotNull(message = "id is required")
   val id: Long,
   @field:NotNull(message = "name is required")
-  val name: String,
-//  @field:EnumValid(enumClass = Status::class, message = "status is invalid")
-//  @EnumNamePattern(regexp = "NEW|DEFAULT")
+  val name: String?,
   @field:ValueOfEnum(enumClass = Status::class, message = "status is invalid")
   val status: String,
 )
--- src/main/kotlin/org/example/springbootenumvalidation/ValidEnum.kt
+++ src/main/kotlin/org/example/springbootenumvalidation/ValidEnum.kt
@@ -4,62 +4,7 @@ import jakarta.validation.Constraint
 import jakarta.validation.ConstraintValidator
 import jakarta.validation.ConstraintValidatorContext
 import jakarta.validation.Payload
-import java.util.regex.Pattern
-import java.util.regex.PatternSyntaxException
 import kotlin.reflect.KClass
-
-
-//@MustBeDocumented
-//@Constraint(validatedBy = [EnumValidator::class])
-//@Target(AnnotationTarget.FUNCTION, AnnotationTarget.FIELD, AnnotationTarget.VALUE_PARAMETER)
-//@Retention(AnnotationRetention.RUNTIME)
-//annotation class ValidEnum(
-//  val message: String = "Invalid value. This is not permitted.",
-//  val groups: Array<KClass<*>> = [],
-//  val payload: Array<KClass<out Payload>> = [],
-//  val enumClass: KClass<out Enum<*>>
-//)
-
-@MustBeDocumented
-@Constraint(validatedBy = [EnumValidator::class])
-@Target(AnnotationTarget.FIELD, AnnotationTarget.VALUE_PARAMETER)
-@Retention(AnnotationRetention.RUNTIME)
-annotation class EnumValid(
-  val enumClass: KClass<out Enum<*>>,
-  val message: String = "invalid value",
-  val groups: Array<KClass<*>> = [],
-  val payload: Array<KClass<out Any>> = []
-)
-@MustBeDocumented
-@Constraint(validatedBy = [EnumNamePatternValidator::class])
-@Target(AnnotationTarget.FUNCTION, AnnotationTarget.FIELD, AnnotationTarget.ANNOTATION_CLASS, AnnotationTarget.CONSTRUCTOR, AnnotationTarget.VALUE_PARAMETER, AnnotationTarget.TYPE)
-@Retention(AnnotationRetention.RUNTIME)
-annotation class EnumNamePattern(
-  val regexp: String,
-  val message: String = "must match \"{regexp}\"",
-  val groups: Array<KClass<*>> = [],
-  val payload: Array<KClass<out Payload>> = []
-)
-class EnumNamePatternValidator : ConstraintValidator<EnumNamePattern, Enum<*>> {
-  private lateinit var pattern: Pattern
-  
-  override fun initialize(annotation: EnumNamePattern) {
-    try {
-      pattern = Pattern.compile(annotation.regexp)
-    } catch (e: PatternSyntaxException) {
-      throw IllegalArgumentException("Given regex is invalid", e)
-    }
-  }
-  
-  override fun isValid(value: Enum<*>?, context: ConstraintValidatorContext): Boolean {
-    if (value == null) {
-      return true
-    }
-    
-    val matcher = pattern.matcher(value.name)
-    return matcher.matches()
-  }
-}
 
 @MustBeDocumented
 @Constraint(validatedBy = [ValueOfEnumValidator::class])
